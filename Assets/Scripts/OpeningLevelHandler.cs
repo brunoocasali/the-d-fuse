286b4f4e2032c84ddc0b13e3f7a74882d3f390e3
brunoocasali <brunoocasali@gmail.com>
2016-12-04 02:28:26 -0200

    auudios

--- Assets/Scripts/OpeningLevelHandler.cs
+++ Assets/Scripts/OpeningLevelHandler.cs
@@ -1,26 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class OpeningLevelHandler : MonoBehaviour {
 
 	public MovieTexture movie;
-	private AudioSource audioSource;
 
 	void Start(){
-		Screen.fullScreen = true;
-		Cursor.visible = false;
-		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), movie, ScaleMode.StretchToFill);
-		movie.Play();
 	}
 
-	void Update () {
-		// Exit the cutscene.
+	void Update() {
 		if (Input.GetKeyUp (KeyCode.Escape) || Input.GetKeyUp (KeyCode.KeypadEnter) && movie.isPlaying) {
-			Screen.fullScreen = false;
-			Cursor.visible = true;
-
-			movie.Stop ();
-			audioSource.Stop ();
+			SceneManager.LoadScene("Menu");
 		}
+	}
+
+	void OnGUI() {
+		movie.Play();
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), movie, ScaleMode.StretchToFill, false, 0.0f);
 	}
 }
